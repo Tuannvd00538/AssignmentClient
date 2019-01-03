f904e0c378c93f9f0b8d17fdedea64c4072c6ca5
hoanghung <hungpvhd00529@fpt.edu.vn>
2019-01-03 16:06:25 +0700

    XuliLogin

--- AssignmentClient/MainPage.xaml.cs
+++ AssignmentClient/MainPage.xaml.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using Newtonsoft.Json;
 using AssignmentClient.Entity;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -19,67 +21,81 @@ namespace AssignmentClient
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
         }
-        private static string API_LOGIN = "https://oauth2servercontext.azurewebsites.net/_api/v1/Authentication/Login";
-
+        private static string API_LOGIN = "https://backendcontroller.azurewebsites.net/_api/v1/Authentication/Login";
         private async void Login_Button(object sender, RoutedEventArgs e)
         {
-            Dictionary<String, String> LoginInfo = new Dictionary<string, string>();
-            LoginInfo.Add("email", this.Email.Text);
-            LoginInfo.Add("password", this.password.Text);
-
             
-       
-            HttpClient httpClient = new HttpClient();
-            StringContent content = new StringContent(JsonConvert.SerializeObject(LoginInfo), System.Text.Encoding.UTF8, "application/json");
-            var response = httpClient.PostAsync(API_LOGIN, content).Result;
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            if (this.Email.Text != "" && this.Password.Password != "")
             {
-                // save file...
-                Debug.WriteLine("Debug Success:" + responseContent);
-                // Doc token
-                TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(responseContent);
+                Dictionary<String, String> LoginInfo = new Dictionary<string, string>();
+                LoginInfo.Add("Email", this.Email.Text);
+                LoginInfo.Add("Password", this.Password.Password.ToString());
 
-                // Luu token
-                StorageFolder folder = ApplicationData.Current.LocalFolder;
-                StorageFile file = await folder.CreateFileAsync("token.txt", CreationCollisionOption.ReplaceExisting);
-                await FileIO.WriteTextAsync(file, responseContent);
 
-                // Lay thong tin ca nhan bang token.
-                HttpClient httpClient2 = new HttpClient();
-                var response2 = new StringContent(JsonConvert.SerializeObject(LoginInfo), System.Text.Encoding.UTF8, "application/json");
-                var content2 = await httpClient.PostAsync(API_LOGIN, response2).Result.Content.ReadAsStringAsync();
-                Debug.WriteLine(content);
-                this.Frame.Navigate(typeof(HomePage));
+                HttpClient httpClient = new HttpClient();
+                StringContent content = new StringContent(JsonConvert.SerializeObject(LoginInfo), System.Text.Encoding.UTF8, "application/json");
+                var response = httpClient.PostAsync(API_LOGIN, content).Result;
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    // save file...
+                    Debug.WriteLine("Debug Success:" + responseContent);
+                    // Doc token
+                    TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(responseContent);
+
+                    // Luu token
+                    StorageFolder folder = ApplicationData.Current.LocalFolder;
+                    StorageFile file = await folder.CreateFileAsync("token.txt", CreationCollisionOption.ReplaceExisting);
+                    await FileIO.WriteTextAsync(file, responseContent);
+
+                    // Lay thong tin ca nhan bang token.
+                    HttpClient httpClient2 = new HttpClient();
+                    var response2 = new StringContent(JsonConvert.SerializeObject(LoginInfo), System.Text.Encoding.UTF8, "application/json");
+                    var content2 = await httpClient.PostAsync(API_LOGIN, response2).Result.Content.ReadAsStringAsync();
+                    Debug.WriteLine(content);
+                    this.Frame.Navigate(typeof(HomePage));
+                }
+                else
+                {
+                    Debug.WriteLine("Debug Error:" + responseContent);
+                    this.error_Password.Text = "username or password is incorrect";
+                    this.error_Password.Visibility = Visibility.Visible;
+                    // //Xu ly loi.
+                    //ErrorResponse errorObject = JsonConvert.DeserializeObject<ErrorResponse>(responseContent);
+                    // if (errorObject != null)
+                    // {
+
+                    // }
+                }
             }
             else
             {
-                Debug.WriteLine("Debug Error:" + responseContent);
-               // //Xu ly loi.
-               //ErrorResponse errorObject = JsonConvert.DeserializeObject<ErrorResponse>(responseContent);
-               // if (errorObject != null)
-               // {
-               //     this.error_UserName.Text = "username or password is incorrect";
-               //     this.error_UserName.Visibility = Visibility.Visible;
-               // }
+                Login_Validate();
             }
+            
 
-
+        }
+        private void Login_Validate()
+        {
             if (this.Email.Text == "")
             {
                 this.error_UserName.Text = "Please enter introduction";
+            }
+            else
+            {
+                this.error_UserName.Visibility = Visibility.Collapsed;
             }
             if (this.Password.Password == "")
             {
                 this.error_Password.Text = "Please enter introduction";
             }
         }
-
         private void Page_loaded(object sender, RoutedEventArgs e)
         {
             //try
@@ -91,7 +107,7 @@ namespace AssignmentClient
             //    {
 
             //        this.Frame.Navigate(typeof(HomePage));
-            //    }
+            //    
             //}
             //catch (FileNotFoundException)
             //{
