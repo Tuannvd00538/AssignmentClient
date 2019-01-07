df05d2efd1c66b5147c63fef520972f4d806e03f
NguyenDanhVuong <vuongndd00547@fpt.edu.vn>
2019-01-07 14:43:28 +0700

    update-dashboard

--- AssignmentClient/Frames/ShowClass.xaml.cs
+++ AssignmentClient/Frames/ShowClass.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using AssignmentClient.Entity;
-using AssignmentClient.Report;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,14 +6,12 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -24,14 +21,12 @@ namespace AssignmentClient.Frames
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Dashboard : Page
+    public sealed partial class ShowClass : Page
     {
-
-
         private List<Classes> Classes;
         private List<Subjects> Subjects;
 
-        public Dashboard()
+        public ShowClass()
         {
             this.InitializeComponent();
             Classes = ClassManager.GetClasses();
@@ -39,11 +34,14 @@ namespace AssignmentClient.Frames
             Subjects = SubjectsManager.GetSubject();
         }
 
-        private void ShowClass(object sender, ItemClickEventArgs e)
+        private void LoadMark(object sender, ItemClickEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
 
-            rootFrame.Navigate(typeof(ShowClass));
+        }
+
+        private void ListSubject(object sender, ItemClickEventArgs e)
+        {
+
         }
     }
 }
