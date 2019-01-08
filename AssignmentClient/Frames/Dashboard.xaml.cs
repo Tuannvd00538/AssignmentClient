813b82dc9e6cfa87b97697701f3db44af472c83e
hoanghung <hungpvhd00529@fpt.edu.vn>
2019-01-08 17:47:24 +0700

    Update..

--- AssignmentClient/Frames/Dashboard.xaml.cs
+++ AssignmentClient/Frames/Dashboard.xaml.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Net.Http;
 using Windows.Data.Json;
 using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -60,6 +61,29 @@ namespace AssignmentClient.Frames
                 ObservableClasses.Add(item);
                 
             }
+        }
+        private void CommandInvokedHandler(IUICommand command)
+        {
+
+        }
+        private async void Mess(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            // Create the message dialog and set its content
+            var messageDialog = new MessageDialog("Chức năng đang phát triển.");
+
+            // Add commands and set their callbacks; both buttons use the same callback function instead of inline event handlers
+            messageDialog.Commands.Add(new UICommand(
+                "Đóng",
+                new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            // Set the command that will be invoked by default
+            messageDialog.DefaultCommandIndex = 0;
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 1;
+
+            // Show the message dialog
+            await messageDialog.ShowAsync();
         }
     }
 }
